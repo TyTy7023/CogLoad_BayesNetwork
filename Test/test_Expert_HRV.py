b919a269bc62f8fc94cb2d74b0ca41a53621b430
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-01-08 15:41:21 +0700

    add expert feature HRV

---
+++ Test/test_Expert_HRV.py
@@ -0,0 +1,68 @@
+#to access files and folders
+import os
+import ast
+from datetime import datetime
+#data analysis and manipulation library
+import pandas as pd
+from argparse import ArgumentParser
+
+import warnings
+warnings.simplefilter("ignore")#ignore warnings during executiona
+
+import sys
+sys.path.append('/kaggle/working/cogload/ProcessData/')
+from ProcessData import Expert_features
+
+#argument parser
+parser = ArgumentParser()
+parser.add_argument("--data_folder_path", default = "/kaggle/input/cognitiveload/UBIcomp2020/last_30s_segments/", type = str, help = "Path to the data folder")
+parser.add_argument("--GroupKFold", default = 3, type = int, help = "Slip data into k group")
+parser.add_argument("--window_size", default = 1, type = int, help = "Window size for feature extraction SMA")
+parser.add_argument("--normalize", default = "Standard", type = str, help = "Normalization method, Standard or MinMax")
+parser.add_argument("--model_selected_feature", default = "None", type = str, help = "None, RFECV, SFS, SBS")
+parser.add_argument("--k_features", default = 11, type = int, help = "k of feature selected of SFS")
+parser.add_argument("--forward", default = False, type = bool, help = "True to use backward, False to use forward")
+parser.add_argument("--floating", default = True, type = bool, help = "True to use sfs with floating, False with no floating")
+parser.add_argument("--split", nargs='+', default=[1] , type=int, help="the split of data example 2 6 to split data into 2 and 6 to extract feature")
+parser.add_argument("--estimator_RFECV", default='SVM', type=str, help="model for RFECV")
+parser.add_argument("--debug", default = 0, type = int, help="debug mode 0: no debug, 1: debug")
+parser.add_argument("--models_single", nargs='+', default=[] , type=str, help="models to train, 'LDA', 'SVM', 'RF','XGB'")
+parser.add_argument("--models_mul", nargs='+', default=[] , type=str, help="models to train, 'MLP_Sklearn', 'MLP_Keras','TabNet'")
+
+args = parser.parse_args()
+
+args_dict = vars(args)
+log_args = pd.DataFrame([args_dict])
+
+directory_name = '/kaggle/working/log/'
+if not os.path.exists(directory_name):
+    os.makedirs(directory_name)
+file_name = f'args.csv'  
+log_args.to_csv(os.path.join(directory_name, file_name), index=False)
+
+#read the data
+label_df = pd.read_excel(args.data_folder_path+'labels.xlsx',index_col=0)
+temp_df= pd.read_excel(args.data_folder_path+'temp.xlsx',index_col=0)
+hr_df= pd.read_excel(args.data_folder_path+'hr.xlsx',index_col=0)
+gsr_df = pd.read_excel(args.data_folder_path+'gsr.xlsx',index_col=0)
+rr_df= pd.read_excel(args.data_folder_path+'rr.xlsx',index_col=0)
+print("Data shapes:")
+print('Labels',label_df.shape)
+print('Temperature',temp_df.shape)
+print('Heart Rate',hr_df.shape)
+print('GSR',gsr_df.shape)
+print('RR',rr_df.shape)
+
+#Expert features
+expert = Expert_features.Expert_feature(temp_df = temp_df,
+                                            hr_df = hr_df,
+                                            gsr_df = gsr_df,
+                                            rr_df = rr_df,
+                                            label_df = label_df,
+                                            window_size = args.window_size,
+                                            normalize = args.normalize)
+hrv = expert.get_hrv_features()
+print('HRV',hrv.shape)
+print('HRV columns',hrv.columns)
+#save the features
+hrv.to_csv('/kaggle/working/hrv.csv',index=False)
--- processData/Expert_features.py
+++ processData/Expert_features.py
@@ -1,19 +1,31 @@
 import pandas as pd
 import numpy as np
 from scipy.signal import resample
-
+import warnings
 import sys
+
+sys.path.append('/kaggle/working/cogload/install_library')
+from install_library import install_and_import
+install_and_import("hrv-analysis")
+from hrvanalysis import get_time_domain_features, get_csi_cvi_features, get_frequency_domain_features, \
+    get_geometrical_features, get_poincare_plot_features
+
 sys.path.append('/kaggle/working/cogload/ProcessData')
 from Processing_Data import Preprocessing
 
-class Expert_GSR(Preprocessing):
+''' HRV functions '''
+HRV_FUNCTIONS = [get_time_domain_features, get_csi_cvi_features,
+                 lambda x: get_frequency_domain_features(x, sampling_frequency=1),
+                 get_geometrical_features, get_poincare_plot_features]
+
+class Expert_feature(Preprocessing):
     def __init__(self, temp_df, hr_df, gsr_df, rr_df, label_df, window_size = 1, normalize = "Standard", data_type='', num_hz = 10):
         super().__init__(temp_df, hr_df, gsr_df, rr_df, label_df, window_size, normalize, data_type)
         self.num_hz = num_hz
         self.stat_feat_all = None
         self.stat_feat_after = pd.concat([temp_df, hr_df, gsr_df, rr_df],axis=1)
 
-    def resample_GSR(self):
+    def resample_eda(self):
         '''
         Resample GSR data to the same length as HR data
         '''
@@ -22,6 +34,41 @@ class Expert_GSR(Preprocessing):
         gsr_resampled = pd.DataFrame(gsr_resampled, columns=self.gsr_df.columns)
         return gsr_resampled
     
+    def get_all_hrv_features(self, ibis):
+        """
+        Wrapper to calculate hrv measures
+        :param ibis: numpy array containing interbeat intervals (in ms)
+        :return: pandas dataframe of hrv features
+        """
+        feats = [func(ibis) for func in HRV_FUNCTIONS]
+        dataframes = []
+        
+        for feat in feats:
+            if isinstance(feat, np.ndarray):  # Nếu là numpy.ndarray
+                dataframes.append(pd.DataFrame(feat).T)  # Chuyển numpy array thành DataFrame
+            elif isinstance(feat, dict):  # Nếu là dictionary
+                dataframes.append(pd.DataFrame.from_dict(feat, orient='index').T)
+        
+        # Gộp các DataFrame lại
+        result = pd.concat(dataframes, axis=1)
+        return result
+
+    def get_hrv_features(self):
+        '''
+        Get HRV features
+        '''
+        # get correct rr intervals in ms
+        correct_rrs = self.rr_df.values * 1000
+        hrv_list = []
+        for i in range(len(correct_rrs)):
+            # get hrv variables
+            warnings.filterwarnings("ignore", category=UserWarning)
+            hrv = self.get_all_hrv_features(correct_rrs[i])
+            hrv = hrv.drop(['mean_hr', 'max_hr', 'min_hr', 'std_hr', 'tinn'], axis=1)
+            hrv.columns = ['hrv__' + c for c in hrv.columns]
+            warnings.filterwarnings("default", category=UserWarning)
+            hrv_list.append(hrv)
+        return pd.concat(hrv_list, axis=0, ignore_index=True).agg(list)
 
 
     
