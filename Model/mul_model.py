72678779024913add3b178a6eaaf9bc324d50bd9
Trần Bùi Ty Ty <147785705+TyTy7023@users.noreply.github.com>
2025-01-10 19:13:46 +0700

    fix get predproba E7GB in multi Model

--- Model/mul_model.py
+++ Model/mul_model.py
@@ -83,7 +83,6 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, n_splits=3 ,
                 estimator.fit(X_train_fold, y_train_fold)
                 y_pred_prob = estimator.predict_proba(X_val_fold)[:,1]
             
-
             else:
                 raise ValueError(f"Model {model} is not supported")
             y_pred_vals.append(y_pred_prob)
@@ -103,7 +102,7 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, n_splits=3 ,
         print(f"Best parameters found: {best_model.best_params}\n")
         y_pred = best_model.predict(X_test)
         y_pred_proba = best_model.predict_proba(X_test)
-        if model == 'MLP_Keras':
+        if model == 'MLP_Keras' or model == 'E7GB':
             y_pred_tests.append(y_pred_proba)
         else: 
             y_pred_tests.append(y_pred_proba[:, 1])
