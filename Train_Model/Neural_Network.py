3b47c00fab918340d5760dcd63cbc88f77013d60
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-01-23 13:41:27 +0700

    fix path and del best param

--- Train_Model/Neural_Network.py
+++ Train_Model/Neural_Network.py
@@ -20,7 +20,7 @@ from RNN import RNNModel
 def train_model(X_train, y_train, X_test, y_test, user_train, path, n_splits=3 , debug = 0, models = ['RNN', 'CNN']):
     np.random.seed(42)
     path = os.path.dirname(path)
-    path_EDA = path + '/EDA/'
+    path_EDA = path + '/EDA/Network_Model/'
   
     log_results = []
     test_accuracy_models = []
@@ -80,7 +80,6 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, n_splits=3 ,
         EDA.draw_ROC(path_EDA + "/models/", y_vals, y_pred_vals, model)
 
         # Dự đoán trên tập kiểm tra
-        print(f"Best parameters found: {best_model.best_params}\n")
         y_pred = best_model.predict(X_test)
         y_pred_proba = best_model.predict_proba(X_test)
         y_pred_tests.append(y_pred_proba)
@@ -105,13 +104,12 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, n_splits=3 ,
         log_results.append({
             "model": model,
             "accuracy": f"{acc} +- {accuracy_all.std()}",
-            "best_model": best_model.best_params,
             "f1_score": f1Score,
             "confusion_matrix": conf_matrix
         })
         print("\n===================================================================================================================================\n")
     log_results = pd.DataFrame(log_results)
-    file_name = f'results_multi_model.csv'  # Tên file tự động
+    file_name = f'results_network_model.csv'  # Tên file tự động
     log_results.to_csv(os.path.join(path, file_name), index=False)
 
     EDA.draw_Bar(path_EDA, models, test_accuracy_models, 'Accuracy Test')
