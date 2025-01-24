4967c8179bbaf2dfbd784a8bbfb39fc0d6e39060
Trần Bùi Ty Ty <147785705+TyTy7023@users.noreply.github.com>
2025-01-24 12:10:43 +0700

    add condition get y_prob E7GB

--- Train_Model/model_fix_param.py
+++ Train_Model/model_fix_param.py
@@ -131,7 +131,7 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, feature_remo
                 best_model = estimator
 
         y_pred = best_model.predict(X_test)
-        if model == 'MLP_Keras':
+        if model == 'MLP_Keras' or model == 'E7GB':
             y_prob = best_model.predict_proba(X_test).flatten()
         else:
             y_prob = best_model.predict_proba(X_test)[:,1]
