84272205a45220d420e3fcdddb1a8f95b7e70448
Trần Bùi Ty Ty <147785705+TyTy7023@users.noreply.github.com>
2025-01-24 13:59:21 +0700

    to list recall pre

--- Train_Model/model_fix_param.py
+++ Train_Model/model_fix_param.py
@@ -138,8 +138,8 @@ def train_model(X_train, y_train, X_test, y_test, user_train, path, feature_remo
 
         # Đánh giá mô hình trên tập kiểm tra
         acc = accuracy_score(y_test, y_pred)
-        precision =[precision_score(y_test, y_pred, average='weighted')
-        recall = recall_score(y_test, y_pred, average='weighted')
+        precision =[precision_score(y_test, y_pred, average='weighted')]
+        recall = [recall_score(y_test, y_pred, average='weighted')]
         f1 = [f1_score(y_test, y_pred, average='weighted')]
         matrix = [confusion_matrix(y_test, y_pred).tolist()]
 
