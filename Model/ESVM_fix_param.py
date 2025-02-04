d836e382a4c2b2b9d160fe6264077ed60954ea3d
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-02-04 23:01:31 +0700

    add experiment and fix esvm fix param

--- Model/ESVM_fix_param.py
+++ Model/ESVM_fix_param.py
@@ -3,18 +3,25 @@ from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.pipeline import Pipeline
-import optuna
 from sklearn.metrics import accuracy_score
 
 class ESVM:
     def __init__(self):
         self.best_model = None
-        self.best_params = None
 
-    def build(self, config):
-    # Tạo pipeline với PCA và AdaBoost
+    def build(self):
+        # Cấu hình cố định
+        config = {
+            'pca__n_components': 35,
+            'svc__C': 0.6295660257538724,
+            'svc__kernel': 'rbf',
+            'svc__gamma': 0.09798977145917381,
+            'adaboost__n_estimators': 87,
+            'adaboost__learning_rate': 0.40967575300614606
+        }
+
         pipeline = Pipeline([
-            ('pca', PCA(n_components=config['pca__n_components'])),  # PCA
+            ('pca', PCA(n_components=config['pca__n_components'])),
             ('adaboost', AdaBoostClassifier(
                 base_estimator=SVC(probability=True, kernel=config['svc__kernel'], C=config['svc__C'], gamma=config['svc__gamma'], random_state=42),
                 n_estimators=config['adaboost__n_estimators'],
@@ -24,38 +31,8 @@ class ESVM:
         ])
         return pipeline
 
-    def training(self, trial, X_train, y_train):
-        # Đề xuất siêu tham số cho SVC, PCA và AdaBoost
-        config = {
-          'pca__n_components': 35,
-          'svc__C': 0.6295660257538724,
-          'svc__kernel': 'rbf',
-          'svc__gamma': 0.09798977145917381,
-          'adaboost__n_estimators': 87,
-          'adaboost__learning_rate': 0.40967575300614606
-      }
-
-        pipeline = self.build(config)
-        pipeline.fit(X_train, y_train)
-        
-        # Đánh giá mô hình trên tập kiểm tra và trả lại độ chính xác để tối ưu hóa
-        y_pred = pipeline.predict(X_train)
-        accuracy = accuracy_score(y_train, y_pred)
-        
-        return accuracy
-
-    def objective(self, trial, X_train, y_train):
-        # Đối tượng tối ưu hóa
-        return self.training(trial, X_train, y_train)
-
     def fit(self, X_train, y_train):
-        # Tạo đối tượng study của Optuna
-        study = optuna.create_study(direction='maximize')  # maximize để tối ưu hóa độ chính xác
-        study.optimize(lambda trial: self.objective(trial, X_train, y_train), n_trials=50)
-
-        # Lấy các tham số tốt nhất từ Optuna
-        self.best_params = study.best_params
-        self.best_model = self.build(self.best_params)
+        self.best_model = self.build()
         self.best_model.fit(X_train, y_train)
 
     def predict(self, X_test):
