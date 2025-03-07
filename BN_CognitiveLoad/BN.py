c0fa8ad253a0516e9f4088ef3a9ef460a0f7670d
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-03-07 18:14:58 +0700

    fix name function

--- BN_CognitiveLoad/BN.py
+++ BN_CognitiveLoad/BN.py
@@ -2,8 +2,8 @@ import sys
 import pandas as pd
 
 sys.path.append('/kaggle/working/cogload/')
-from install_library import install_library
-install_library('pgmpy')
+from install_library import install_and_import
+install_and_import('pgmpy')
 
 from sklearn.preprocessing import KBinsDiscretizer
 
