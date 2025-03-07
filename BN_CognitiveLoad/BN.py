6e490b880f3fa7fac79a3a7b968ed73ae987c881
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-03-07 21:53:12 +0700

    fix fit edges

--- BN_CognitiveLoad/BN.py
+++ BN_CognitiveLoad/BN.py
@@ -66,7 +66,8 @@ class BayesianNetwork:
         
         accuracies = []
         print("Edges of DAG:", self.edges)
-        model = BayesianNetwork(self.edges)
+        edges = self.edges
+        model = BayesianNetwork(edges)
         best_acc = 0
         self.best_model = None
 
