93dcee2fc1fa390a001a96c1b1933d62a9f7dfc3
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-03-11 15:17:36 +0700

    debug

--- Test/test_BN_Model.py
+++ Test/test_BN_Model.py
@@ -50,23 +50,15 @@ selected_features = ['temp_mean','temp_std','temp_max-min','temp_skew','temp_kur
                      'Labels']  # Thay thế bằng các đặc trưng bạn muốn giữ lại
 data = data[selected_features]
 
-process = Processing(data, label_df)
+process = Processing(data, label_df, directory_name)
 data = process.Discretization_of_data(bins = 2)
 
-# create new mean features
-# Xác định nhóm đặc trưng
-feature_groups = {
-    "temp_features": ['temp_mean', 'temp_std', 'temp_max-min', 'temp_skew', 'temp_kurtosis'],
-    "hr_features": ['hr_mean', 'hr_std', 'hr_max-min', 'hr_skew', 'hr_kurtosis'],
-    "rr_features": ['rr_mean', 'rr_std', 'rr_max-min', 'rr_skew', 'rr_kurtosis', 
-                    'HRV_Cd', 'HRV_AI',  'HRV_MedianNN', 'HRV_GI', 'HRV_CSI_Modified', 
-                    'HRV_CVNN', 'HRV_RMSSD', 'rr_diff2', 
-                    'HRV_IALS', 'HRV_PAS', 'HRV_HFn'],
-    "gsr_features": ['gsr_mean', 'gsr_std', 'gsr_max-min', 'gsr_skew', 'gsr_kurtosis']
-}
-
-for feature_name, columns in feature_groups.items():
-    data[feature_name] = data[columns].mean(axis=1, skipna=True)
+# create mean features
+data = pd.read_csv(f'{directory_name}discrete_data.csv')
+data['temp_features'] = data[['temp_mean', 'temp_std', 'temp_max-min', 'temp_skew', 'temp_kurtosis']].mean(axis=1)
+data['hr_features'] = data[['hr_mean', 'hr_std', 'hr_max-min', 'hr_skew', 'hr_kurtosis']].mean(axis=1)
+data['rr_features'] = data[['rr_mean', 'rr_std', 'rr_max-min', 'rr_skew', 'rr_kurtosis','HRV_Cd','HRV_AI','HRV_MedianNN','HRV_GI','HRV_CSI_Modified','HRV_CVNN','HRV_RMSSD','rr_diff2','HRV_IALS','HRV_PAS','HRV_HFn']].mean(axis=1) 
+data['gsr_features'] = data[['gsr_mean', 'gsr_std', 'gsr_max-min', 'gsr_skew', 'gsr_kurtosis']].mean(axis=1)
 
 #Processing data
 process = Processing(data, label_df)
@@ -74,7 +66,6 @@ X_train, y_train, X_test, y_test, user_train, user_test = process.get_Data(bins 
 
 # Draw DAG
 edges_list = ast.literal_eval(f"[{args.edges}]")
-print(edges_list)
 bn = BN(data, method=args.method, edges = edges_list)
 bn.fit(X_train, y_train, user_train, args.GroupKFold)
 accuracy = bn.predict(X_test, y_test)
